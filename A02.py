69910aa17045d14e678ead18774463c775b702cf
Cristian <duquec@sunypoly.edu>
2024-10-05 14:07:36 -0400

    Setup of A02

---
+++ A02.py
@@ -0,0 +1,21 @@
+## Information on Code
+## Assignment 02 of CS 490 Video Processing and Vision
+## Purpose: Compute optical flow. Works in tandem with A01.py
+## Top-Down Approach
+## Writer: Cristian Duque
+## Based off material from Professor Reale
+
+###############################################################################
+# IMPORTS
+###############################################################################
+
+from pathlib import Path
+import sys
+import cv2
+import os 
+import shutil
+
+
+###############################################################################
+# Definitions of functions
+###############################################################################
