56d8b67976a4b52a9d10b121bbb4a31f11971b75
Cristian <duquec@sunypoly.edu>
2024-12-03 15:24:43 -0500

    commit

---
+++ A04.py
@@ -0,0 +1,62 @@
+# Information on Code
+# Assignment 04 of CS 490 Video Processing and Vision
+# 
+# Purpose: Goal of this assignment is to write code to 
+# train and evaluate PyTorch neural network models to 
+# predict classes from a SUBSET of the HMDB51 dataset.
+# 
+# Writer: Cristian Duque
+# - Based off material from Professor Reale
+
+###############################################################################
+# IMPORTS
+###############################################################################
+
+from pathlib import Path
+import sys
+import cv2
+import os 
+import shutil
+
+
+###############################################################################
+# Definitions of functions
+###############################################################################
+
+# Function that returns a list of the names of all combinations
+# that will be tested. Makes use of self-documented names.
+def get_approach_names():
+    
+ return 
+
+# Function that given the approach name returns a text
+# description of what makes this approach distinct. No 
+# more than 1 sentence in terms of length.
+def get_approach_description(approach_name):
+    
+ return 
+
+# Function that when given approach name and if told it is
+# training data, returns the appropriate dataset transform
+# Does NOT augment data for non-training data.
+def get_data_transform(approach_name, training):
+    
+ return 
+
+# Function that given the approach name, returns the preferred
+# batch size. This can be hardcoded to a single value if there
+# is no preference, but allows one to use a smaller batch size
+# if the architecture is large enough where that is a concern.
+def get_batch_size(approach_name):
+    
+ return
+
+# Function that 
+def create_model(approach_name, class_cnt):
+    
+ return 
+
+# Function that 
+def train_model(approach_name, model, device, train_dataloader, test_dataloader):
+    
+ return 
