8e9edcb704fbf75c3c7897ec9a4d3d6c0fcaee8e
Cristian <duquec@sunypoly.edu>
2024-12-04 13:26:45 -0500

    Development of transform function

--- A04.py
+++ A04.py
@@ -13,10 +13,16 @@
 ###############################################################################
 
 from pathlib import Path
-from pytorchvideo.models.hub import slow_r50
-import os 
 import shutil
-
+import torch
+import torchvision
+from torch import nn
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets
+from torchvision.transforms import v2
+import cv2
+import numpy as np
+import os
 
 ###############################################################################
 # Definitions of functions
@@ -32,15 +38,29 @@ def get_approach_names():
 # description of what makes this approach distinct. No 
 # more than 1 sentence in terms of length.
 def get_approach_description(approach_name):
+    descrip = {
+        "SimpleCNN": "Basic CNN with 3 Convolutional layers",
+        "ResNetTransferLearning": "ResNet-based model pre-trained on ImageNet, fine-tuned on HMDB51"
+    }
     
- return 
+    return descrip.get(approach_name, "Unknown approach.")
+
+
 
 # Function that when given approach name and if told it is
 # training data, returns the appropriate dataset transform
 # Does NOT augment data for non-training data.
 def get_data_transform(approach_name, training):
-    
- return 
+    # Checks to see if this is for transformation is for data training
+    if training:
+        # For training data, applies data augmentation
+        data_transforms = v2.Compose([v2.ToImageTensor(), 
+                                  v2.ConvertImageDtype()])
+    else:
+        # For non-training data, only necessary transformations (no augments)
+        data_transforms = v2.Compose([v2.ToImageTensor(), 
+                                  v2.ConvertImageDtype()])
+    return data_transforms
 
 # Function that given the approach name, returns the preferred
 # batch size. This can be hardcoded to a single value if there
